54152cb260faf80e53615f5daf2c7bbdaf745766
alyonachern <alenaisakova@mail,ru>
2024-03-14 22:46:46 +0300

    deleted unused import of GetRedirectUrlTestTest

--- src/test/java/GetRedirectUrlTest.java
+++ src/test/java/GetRedirectUrlTest.java
@@ -2,8 +2,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.awt.image.RescaleOp;
-
 public class GetRedirectUrlTest {
 
     @Test
